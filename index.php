feb7d7a4d56b07c85718a860d474b93b79d12214
Jesslyn Yang <jesslynyangwebsite@gmail.com>
2020-06-20 17:58:17 +0700

    Menambahkan list

--- index.php
+++ index.php
@@ -7,5 +7,9 @@
 <body>
 	<h1>Hello World!</h1>
 	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto, voluptatem!</p>
+	<ul>
+		<li>satu</li>
+		<li>dua</li>
+	</ul>
 </body>
 </html>
